2c02f36ebdff5d30524a492489147eea47ba0458
Abhi2609 <121375083+Abhi2609@users.noreply.github.com>
2024-06-28 05:06:23 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // different syntax to use in node not this way as used in browsers
+
+console.log(3+3) 
+
+// use ECMA Script and MDN to see the documentation ---> JS standard
+
+/*
+    DATA TYPES:
+        number => 2^53
+        bigint => for bigger numbers
+        string => ""
+        boolean => true or false
+        null => standalone value represents empty
+        undefined => no defined value assigned
+        symbol => uniqueness
+
+
+        object
+*/
+
+console.log(typeof undefined) // ---> undefined
+console.log(typeof null)      // ---> object
